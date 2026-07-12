ff04876e0f94c40fba9a4762993ce69ffdddc20a
thesakshidiggikar <sakshidiggikar681@gmail.com>
2026-07-12 18:58:41 +0530

    feat(database): add SQLAlchemy models and database configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from app.core.config import settings
-print(settings.DATABASE_URL)
+
+from app.database.database import engine
+from app.database.base import Base
+
 
 app= FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def home():
-    return {"message": "Backend is running successfully"}
+    return {
+        "message": "Backend is running successfully"
+        }
---
+++ backend/app/models/directory.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from sqlalchemy import DateTime , String 
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List,TYPE_CHECKING
+from app.database.database import Base 
+if TYPE_CHECKING:
+    from app.models.todo import Base
+class Directory(Base):
+    __tablename__="directories"
+    id:Mapped[int]=mapped_column(primary_key=True,index=True)
+    name:Mapped[str]=mapped_column(String(100),
+                                   nullable=False)
+    
+    created_at:Mapped[datetime]=mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    
+    )
+
+    updated_at:Mapped[datetime]=mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+
+    todos:Mapped[List["Todo"]]=relationship(
+        back_populates="directory",
+        cascade="all, delete-orphan",
+    )
